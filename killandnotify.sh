3b01b4d12aabf161eabd5b0ed7adca85219d55ab
entnuo <neptunemoon051@gmail.com>
2022-04-11 18:27:34 -0300

    Added mouse-sets and minor change in kan.sh

--- killandnotify.sh
+++ killandnotify.sh
@@ -2,6 +2,6 @@
 # Kills an application and sends a notification that it's been killed
 # Created by BrodieRobertson, stealed and modified by me
 
-KILL=$( rofi -dmenu -p "Which process would you like to kill?" )
+KILL=$( rofi -dmenu -width 650 -p "Which process would you like to kill?" )
 
 killall $KILL && notify-send "$KILL was killed"
