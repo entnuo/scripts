860f41e1b3ce6f55a2e99f0423ceb525ab592212
entnuo <neptunemoon051@protonmail.com>
2023-04-04 12:39:52 -0300

    killandnotify.sh removed

--- killandnotify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Kills an application and sends a notification that it's been killed
-# Created by BrodieRobertson, stealed and modified by me
-
-KILL=$( rofi -dmenu -width 650 -p "Which process would you like to kill?" )
-
-killall $KILL && notify-send "$KILL was killed"
